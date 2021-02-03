fb37f3710538d8a7cb2ca4a024773d27f65c432b
Matt Ellison <mattbryanellison@gmail.com>
2021-02-03 15:09:01 -0500

    [adds] search user endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,24 @@
-// const router = require("express").Router({ mergeParams: true });
-// const { Op } = require("sequelize");
-// const Test = require("../db/test");
-// module.exports = router;
+const router = require("express").Router({ mergeParams: true });
+const { Op } = require("sequelize");
+const User = require("../db/models/user");
+module.exports = router;
 
-// // /api/users/search?name=<NAME>
-// router.get("/search", async (req, res, next) => {
-//   try {
-//     console.log("HEY GURL");
-//     console.log(req.query);
-//     if (req.query.name) {
-//       console.log(req.query.name);
-//       let users = await Test.findAll({
-//         where: { name: { [Op.iLike]: `${req.query.name}%` } },
-//       });
-//       res.json(users);
-//     }
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-// // /api/users/
-// router.post("/", async (req, res, next) => {
-//   try {
-    
-//     console.log(req.body);
-//     const newUser = await Test.create(req.body);
-//     res.json(newUser);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+// /api/users/search?name=<NAME>
+router.get("/search", async (req, res, next) => {
+  try {
+    if (req.query.name) {
+      console.log(req.query.name);
+      let users = await User.findAll({
+        where: {
+          [Op.or]: [
+            { firstName: { [Op.iLike]: `%${req.query.name}%` } },
+            { lastName: { [Op.iLike]: `%${req.query.name}%` } },
+          ],
+        },
+      });
+      res.json(users);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
